a8c2b5d84805f1e17bd5dc347b84a7ec6b4df7c9
Anton Nemtsev <anton.nemtsev@i3d.net>
2023-05-29 16:21:32 +0200

    logging

--- scripts/index.js
+++ scripts/index.js
@@ -13,18 +13,32 @@ const STUDENTS_PATH = 'configuration/students.json';
 (async () => {
   // Reading configuration files
   const CALENDAR = JSON.parse(fs.readFileSync(CALENDAR_PATH, 'utf8'));
+  console.log(CALENDAR);
   const STUDENTS = JSON.parse(fs.readFileSync(STUDENTS_PATH, 'utf8'));
+  console.log(STUDENTS);
+
   const octokit = new Octokit({ 
     auth: process.env.PRIVATE_KEY,
   });
 
   STUDENTS.forEach(username => {
+    console.log(username);
+
     Object.entries(CALENDAR).forEach(async ([repository, dateString]) => {
+
       const availabilityDate = new Date(dateString);
       const NEW_REPO = `${username}-${repository}`;
       const hasRepository = await doRepositoryExists(octokit)(NEW_REPO);
+      console.log({
+        repository, 
+        dateString,
+        NEW_REPO,
+        hasRepository,
+      });
       if (isDateBeforeToday(availabilityDate) && !hasRepository) {
-        await cloneFromTemplate(octokit)(repository, username);
+        console.log('creating');
+        const result = await cloneFromTemplate(octokit)(repository, username);
+        console.log(result);
       }
     });
   });
